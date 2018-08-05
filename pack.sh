88b05c363aa47c98bcefe440e0390edf5f2a86bf
Israel Dago <dagoisrael@gmail.com>
2018-08-05 22:08:24 +0300

    added extern web service for managing users registration

--- pack.sh
+++ pack.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-./mvnw install:install-file -Dfile=./externLib/app-users-ws-0.0.1-SNAPSHOT.jar -DgroupId=com.israeldago \
-        -DartifactId=app-users-ws -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 ./mvnw clean package
