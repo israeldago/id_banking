ab3daf7bd55158f1652fa21350a978e151dc5452
Israel Dago <dagoisrael@gmail.com>
2018-08-06 22:24:25 +0300

    added new ErrorCode

--- common/src/main/java/com/israeldago/idBanking/common/enums/ErrorCode.java
+++ common/src/main/java/com/israeldago/idBanking/common/enums/ErrorCode.java
@@ -6,7 +6,8 @@ import static org.springframework.http.HttpStatus.*;
 
 public enum ErrorCode {
     TECHNICAL(INTERNAL_SERVER_ERROR),
-    USER_NOT_FOUND(NOT_FOUND);
+    USER_NOT_FOUND(NOT_FOUND),
+    BAD_CREDENTIALS(HttpStatus.FORBIDDEN);
 
     private final HttpStatus status;
 
