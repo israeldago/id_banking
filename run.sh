7f21547e03ba921ebbd7519ed55f7e3762e699dc
Israel Dago <dagoisrael@gmail.com>
2018-08-12 19:36:45 +0300

    refactoring to use 2 main modules (backend&frontend)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-gnome-terminal --tab -e "java -Dserver.port=8083 -jar web/target/id_banking-0.0.1-SNAPSHOT.jar" &&
-java -jar ./externLib/app-users-ws-0.0.1-SNAPSHOT.jar
+gnome-terminal --tab -e "java -Dserver.port=8083 -jar frontend/target/id_banking-0.0.1-SNAPSHOT.jar" &&
+java -jar ./backend/externLib/app-users-ws-0.0.1-SNAPSHOT.jar
