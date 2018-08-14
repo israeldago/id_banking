c8c13a0f2c2538b0a40285c1e1cfa3fd77b1ea04
Israel Dago <dagoisrael@gmail.com>
2018-08-15 02:07:19 +0300

    configured backend auth url

--- frontend/src/main/java/com/israeldago/idBanking/frontend/configuration/FrontendConfig.java
+++ frontend/src/main/java/com/israeldago/idBanking/frontend/configuration/FrontendConfig.java
@@ -1,17 +1,23 @@
 package com.israeldago.idBanking.frontend.configuration;
 
 import com.israeldago.idBanking.backend.itf.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
 
 @Configuration
+@PropertySource("classpath:presentation.properties")
 public class FrontendConfig {
+
+    @Value("${remoting.authentication.url}")
+    private String backendAuthServiceUrl;
 
     @Bean(name = "authServiceHttpInvoker")
     public HttpInvokerProxyFactoryBean httpInvoker() {
         HttpInvokerProxyFactoryBean invoker = new HttpInvokerProxyFactoryBean();
-        invoker.setServiceUrl("http://localhost:8080/authService");
+        invoker.setServiceUrl(backendAuthServiceUrl);
         invoker.setServiceInterface(AuthenticationService.class);
         return invoker;
     }
